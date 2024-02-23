7c43d45b9a65c5b36ae3140ef5fd28efc682726a
Danushka-Madushan <Danushkamadushan128@gmail.com>
2024-02-23 23:24:28 +0530

    update src/main/index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -46,7 +46,7 @@ const createWindow = (): void => {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.musix')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
