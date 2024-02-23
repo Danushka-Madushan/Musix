34b3118ccac789f1a707f3d97c3957c7801bebac
Danushka-Madushan <Danushkamadushan128@gmail.com>
2024-02-23 23:52:41 +0530

    update src/preload/index.ts

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,7 +1,6 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
 const api = {}
 
 // Use `contextBridge` APIs to expose Electron APIs to
