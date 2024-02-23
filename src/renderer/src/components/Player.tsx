010a1741d2d32e04db3b8fd4714f3db48d24162b
Danushka-Madushan <Danushkamadushan128@gmail.com>
2024-02-23 22:46:46 +0530

    update Player.tsx

--- src/renderer/src/components/Player.tsx
+++ src/renderer/src/components/Player.tsx
@@ -182,6 +182,7 @@ const Player = ({ songContent, setSongContent, Playlist }: PlayerProps): JSX.Ele
                     <path fill="currentColor" d="M5 15V9h4l5-5v16l-5-5zm11 1V7.95q1.125.525 1.813 1.625T18.5 12q0 1.325-.687 2.4T16 16" />
                   </svg>
                   <input
+                    disabled={isMuted}
                     className='cursor-pointer'
                     type="range"
                     min={0}
